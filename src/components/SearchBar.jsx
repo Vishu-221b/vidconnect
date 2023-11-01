7417a07b226ce162fdabaf08b20a97dc108bd671
Vishu-221b <vishwajeetsinghchauhan63@gmail.com>
2023-11-01 12:49:58 +0530

    minor fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ const SearchBar = () => {
 
         if(searchTerm){
             navigate(`/search/${searchTerm}`);
-            setSearchTerm('hehe')
+            setSearchTerm('')
         }
     }
 
